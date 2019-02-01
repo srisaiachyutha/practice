7483e6b4a45c4f2fcfd50cba03e4651983edebd0
punna sri sai achyutha <44301170+srisaiachyutha@users.noreply.github.com>
2019-02-01 20:53:39 +0530

    Add files via upload

--- spiral.py
+++ spiral.py
@@ -3,7 +3,19 @@
 #   1   2    3
 #   8   9    4
 #   7   6    5
-
+c=1
+def prime():
+    global c
+    while True:
+        k=0
+        for i in range(1,c+1):
+            if c%i==0:
+                k+=1
+        if k==2:
+            c+=1
+            return c-1
+        else:
+            c+=1
 
 n=int(input('enter the size of the matrix'))
 m=[]
@@ -16,29 +28,29 @@ for i in range(n):
 k=n
 if n%2!=0:
     k=n+1
-c=1
+
 for l in range(k//2):
     i,j=l,l
     for j in range(l,n-l):
-        m[i][j]=c
-        c+=1
+        m[i][j]=prime()
+        
 
     j=n-1-l
     for i in range(l+1,n-l-1):
-        m[i][j]=c
-        c+=1
+        m[i][j]=prime()
+        
 
     j=n-1-l
     i=n-1-l
     for j in range(n-1-l,l,-1):
-        m[i][j]=c
-        c+=1
+        m[i][j]=prime()
+        
 
     j=l
     
     for i in range(n-1-l,l,-1):
-        m[i][j]=c
-        c+=1
+        m[i][j]=prime()
+        
 
 
 for i in range(n):
