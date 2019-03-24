1d93d39bff2d18e7b0e4b74b7473d7192dc2952c
punna sri sai achyutha <44301170+srisaiachyutha@users.noreply.github.com>
2019-03-24 17:02:10 +0530

    Add files via upload

--- minegame.py
+++ minegame.py
@@ -1,24 +1,21 @@
 import random
-import time
 def display(matrix,matrix2):
-    print('  ',end='')
-    for _ in range(10):
-        #print(' ',end='')
-        print(_,'\t',end=" ")
-    print()
-    for i in range(len(matrix)):
+    '''this will display the matrix format of the game'''
+    print('   0  1  2  3  4  5  6  7  8  9  ')
+    for i in range(10):
         print(i,' ',end='')
-        for j in range(len(matrix)):
+        for j in range(10):
             if matrix2[i][j]==1:
-                print(' ',matrix[i][j],'\t',end="")
+                print(matrix[i][j],' ',end="")
             else:
-                print('  ','\t',end='')
+                print(' ',' ',end='')
+            
         print()
 matrix1=[[0]*10 for _ in range(10)]
 matrix2=[[-1]*10 for _ in range(10)]
-
-
+cou=0
 def create(matrix):
+    '''this function will make the bombs count ready in the matrix'''
     for i in range(10):
         for j in range(10):
             c=0
@@ -65,15 +62,12 @@ def create(matrix):
                     matrix[i][j]=c
                 else:
                     matrix[i][j]='-'
-    
-'''for i in range(10):
-    m=[]
-    for j in range(10):
-        m.append(0)
-    matrix1.append(m)
-'''
+ 
 def start(matrix1,matrix2,i,j):
+    '''this function is the main function which will reveal the boxes'''
     matrix2[i][j]=1
+    global count
+    count-=1
     if matrix1[i][j]=='-':
         x=i-1
         y=j-1
@@ -113,26 +107,24 @@ def start(matrix1,matrix2,i,j):
         y=j+1
         if x>=0 and y>=0 and x<10 and y<10 and matrix2[x][y]!=1:
             start(matrix1,matrix2,x,y)
-
-    
-    
-
-
-#matrix=[[' ']*10]*10
-#print(matrix1)
 c=10
 while c>0:
     
     x=random.randint(0,9)
-    #print(a)
+   
     y=random.randint(0,9)
-    #print(x,y)
     matrix1[x][y]='b'
     c-=1
+
 create(matrix1)
-count=100
+
+for i in matrix1:
+    cou+=i.count('b')
+count=100-cou
+print('the value of the count is ',count)
+#count=100
 win=True
-while count>90:
+while count>0:
     print('enter the x and y coordinates\n')
     n=int(input())
     m=int(input())
@@ -141,11 +133,13 @@ while count>90:
         break
     start(matrix1,matrix2,n,m)
     display(matrix1,matrix2)
+     
 if win:
     print('you have won the game')
 else:
-    for i in range(10):
+    
+    print('you have lost it')
+for i in range(10):
         for j in range(10):
             matrix2[i][j]=1
-    display(matrix1,matrix2)
-    print('you have lost it')
+display(matrix1,matrix2)
