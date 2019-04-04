57c2e79b1f5f6280a26f9ef2c8c0c455a23433ae
punna sri sai achyutha <44301170+srisaiachyutha@users.noreply.github.com>
2019-04-04 11:56:17 +0530

    Update love.py

--- love.py
+++ love.py
@@ -4,7 +4,7 @@ def loveFunction():
         s=''
         for x in range(-30,30):
             if ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0:
-                s+=lover[(x-y)%8]
+                s+=lover[(x-y)%len(lover)]
             else:
                 s+=' '
         print(s)
